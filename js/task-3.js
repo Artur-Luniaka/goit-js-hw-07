97922c13a443d2b49f96b99e3058c487fe9baf83
Artur Luniaka <artur01049718@gmail.com>
2024-08-17 18:54:49 +0100

    finish task-03 and task-04

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('#name-input')
+const span = document.querySelector('#name-output')
+input.addEventListener("input", function () {
+    if (input.value.trim() === '') {
+        span.textContent = "Anonymous"
+    } else {
+        span.textContent = input.value
+    }
+})
+
+
+
